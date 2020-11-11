0daa11a14af2cb1834b6bad6ccbf82d1bb396259
Claire Casey <clcasey805@gmail.com>
2020-11-11 15:28:42 -0800

    solving average-pair problem

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,23 @@
+"use strict";
+
 // add whatever parameters you deem necessary
-function averagePair() {
+
+/* 
+Function accepts a sorted array of integers and average and returns a Boolean if you can calculate average from a pair of integers in array.
+*/ 
+function averagePair(nums, targetAvg) {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low < high) {
+        let checkAvg = (nums[low] + nums[high]) / 2;
+        if (checkAvg === targetAvg) return true;
+        if (checkAvg > targetAvg) {
+            high --;
+        }
+        else if (checkAvg < targetAvg) {
+            low ++;
+        }
+    }
+    return false;
 }
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,33 +1,34 @@
+"use strict";
 // add whatever parameters you deem necessary & write doc comment
 /* 
  * accepts a word and a string of letters and returns a Boolean if the word can be constructed from the letters
- * all the letters are lowercased, no special characters
+ * assume all the letters are lowercased, no special characters
  */
 function canConstructWord(word, letters) {
-    // if letters are empty and word is empty then thats valid right?
+    // if word is empty, you can create it from any set of letters, so return true
     if (!word) return true;
     // if given no letters, cannot construct a word
     if (!letters) return false;
 
-    let wordFrequencies = buildFrequencyCounter(word);
+    let wordLtrFrequencies = buildFrequencyCounter(word);
     let lettersFrequencies = buildFrequencyCounter(letters);
 
-    console.log("word freqs:", wordFrequencies);
+    console.log("word freqs:", wordLtrFrequencies);
     console.log("letter freqs:", lettersFrequencies);
-    // goal: go thru letter of the word, and check that the frequencies in our letters string is >=  the frequency of the letter in that word
-    for (let key of  wordFrequencies.keys()) {
+    // go thru letter of the word, and check that the frequencies in our letters string is >= the frequency of the letter in that word
+    for (let key of wordLtrFrequencies.keys()) {
         // check that letterFrequencies even has this key
-        if (!lettersFrequencies.has(key)) { 
+        if (!lettersFrequencies.has(key)) {
             return false;
         }
 
-        if (lettersFrequencies.get(key) < wordFrequencies.get(key)) {
+        if (lettersFrequencies.get(key) < wordLtrFrequencies.get(key)) {
             return false;
         }
     }
 
     return true;
-    
+
 
 }
 
@@ -35,13 +36,13 @@ function canConstructWord(word, letters) {
  * build a frequency counter for letters of each input string
  */
 
- function buildFrequencyCounter(letters) {
-     let frequencies = new Map();
+function buildFrequencyCounter(letters) {
+    let frequencies = new Map();
 
-     for (let letter of letters) {
-         let valCount = frequencies.get(letter) || 0;
-         frequencies.set(letter, valCount + 1);
-     }
-     return frequencies;
- }
+    for (let letter of letters) {
+        let valCount = frequencies.get(letter) || 0;
+        frequencies.set(letter, valCount + 1);
+    }
+    return frequencies;
+}
 
