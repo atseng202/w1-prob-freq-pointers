3a4b39418d52f2856665459ab195ff67866c8f63
Alan Tseng <atseng202@gmail.com>
2020-11-11 15:54:23 -0800

    Complete same frequency and two array obj problems

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,31 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+/* 
+ * function accepts two integers and returns true if the frequency of digits in each one is the same, false otherwise
+ * Expected time complexity: O(n + m)
+ */
+function sameFrequency(digit1, digit2) {
+    // convert both digits into strings, build their frequency counters, and check that the counters are the same for each digit
+    let digit1Freqs = buildFreqs(digit1);
+    let digit2Freqs = buildFreqs(digit2);
+
+    for (let oneDigit of digit1Freqs.keys()) {
+        if (!digit2Freqs.has(oneDigit)) return false;
+
+        if (digit1Freqs.get(oneDigit) !== digit2Freqs.get(oneDigit)) return false;
+    }
+
+    return true;
+}
+
+/* 
+    Helper function: build frequencies map for digits
+ */
+function buildFreqs(digit) {
+    let digitStr = digit.toString();
+    let frequencies = new Map();
+    for (let oneDigit of digitStr) {
+        let valCount = frequencies.get(oneDigit) || 0;
+        frequencies.set(oneDigit, valCount + 1);
+    }
+    return frequencies;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,22 @@
+"use strict";
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+/* 
+ * function takes in an array of keys and an array of values
+ * it returns an object created from the keys and values
+ * if the number of keys are > number of values, the remaining keys should map to null
+ * if number of values > number of keys, ignore them
+ * 
+ * we assume all keys are unique, so values arent replaced
+ */
+function twoArrayObject(keysArr, valuesArr) {
+    // loop through every index of the keys array
+    // grab the corresponding value at that index
+    // for indices that are longer than the values arr, the key-value pair is just null
+    let keyValObj = {};
+    for (let i = 0; i < keysArr.length; i++) {
+        const key = keysArr[i];
+        const val = valuesArr[i] || null;
+        keyValObj[key] = val;
+    }
+    return keyValObj;
 }
