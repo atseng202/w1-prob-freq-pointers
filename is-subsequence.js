5c64c398067916de225474fe555d7e924e28351f
Claire Casey <clcasey805@gmail.com>
2020-11-11 16:34:38 -0800

    complete is-subsequence problem and separate-positive

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,24 @@
+"use strict"
 // add whatever parameters you deem necessary
-function isSubsequence() {
+/* 
+Fumction takes two strings and checks if the first string is a subsequence of the second string. 
+Order matters, but letters in between the subsequence is okay. 
+The function returns a true if the first string is a subsequence and false if not.
+*/ 
+function isSubsequence(subString, bigString) {
+
+    let foundCount = 0;
+    let newStartIndex = 0;
+
+    for (let i = 0; i < subString.length; i ++) {
+        for (let j = newStartIndex; j < bigString.length; j++) {
+            if (subString[i] === bigString[j]) {
+                newStartIndex = j+1;
+                foundCount ++;
+                break;
+            }
+        }
+    }
+    //return true if we found all characters in subString
+    return (foundCount == subString.length);
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+"use strict"
+
+/* 
+function takes in an array of integers and returns the same array with positive numbers on the left and negative on the right. 
+The positive and negative numbers will not necessarily be sorted.
+We are assuming there are no zeros in input array.
+*/ 
+function separatePositive(nums) {
+    let negativePointer = 0;
+    let positivePointer = nums.length -1;
+
+    while (negativePointer < positivePointer) {
+        // if the value on the left side is negative, swap with the value on the right pointer and move right pointer one to the left. 
+        if (nums[negativePointer] < 0) {
+            [nums[negativePointer], nums[positivePointer]] = [nums[positivePointer], nums[negativePointer]];
+            positivePointer --; 
+        }
+        // otherwise, move left pointer one to the right.
+        else {
+            negativePointer ++;
+        }
+    }
+    return nums;
 }
